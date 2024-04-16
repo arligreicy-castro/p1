80adbcc7beee243591dc21a3514cface460ff324
arligreicy <arligreicy.castro@gmail.com>
2024-04-16 10:21:53 -0300

    estruturando uma

--- form.class.php
+++ form.class.php
@@ -1,6 +1,24 @@
 <?php
     class form implements Componente
     {
-       
+        public function __construct(private string $action = "",
+        private string $method = "",private array $elementos = array()){}
+
+        public function criar()
+        {
+            echo "<form action='{$this->action}'method='{$this->method}'>";
+
+                foreach($this->elementos as $objeto)
+                {
+                    $objeto->criar();
+                }
+
+            echo "</form>";
+            
+        }
+        public function setElementos($elemento)
+        {
+            $this->elementos[] = $elemento;
+        }
     }
 ?>
