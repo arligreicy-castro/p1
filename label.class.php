27d0c5cce6510717e77bffe54290b8adf5d092ff
arligreicy <arligreicy.castro@gmail.com>
2024-04-16 10:25:00 -0300

    estruturando input e label

--- label.class.php
+++ label.class.php
@@ -1,6 +1,12 @@
 <?php
     class label implements Componente
     {
-        
+        public function __construct(private string $for = "",
+        private string $texto = ""){}
+
+        public function criar()
+        {
+            echo "<label for='{$this->for}'>{$this->texto}</label>";
+        }
     }
 ?>
